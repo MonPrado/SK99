02647bb7e873cc52fddc3a9d8a224adc829cb3c4
MonPrado <104338049+MonPrado@users.noreply.github.com>
2024-05-09 18:47:40 -0300

    Add files via upload

---
+++ scripts/adicionar-encomenda.js
@@ -0,0 +1,118 @@
+document.getElementById("produtoForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Limpa mensagens de erro anteriores
+    document.getElementById("error-messages").innerHTML = '';
+
+    // Captura dos valores do formulário
+    var nome = document.getElementById("nome").value;
+    var quantidade = parseInt(document.getElementById("quantidade").value);
+    var produto = document.getElementById("produto").value;
+    var valor = parseFloat(document.getElementById("valor").value);
+
+     // Limpa mensagens de erro anteriores
+     limparMensagensErro();
+
+     // Array para armazenar mensagens de erro
+     var mensagensErro = [];
+ 
+     // Verificação da validade da quantidade e do valor
+     if (quantidade <= 0 || isNaN(quantidade)) {
+         mensagensErro.push("Quantidade inválida.");
+     }
+     
+     if (valor <= 0 || isNaN(valor)) {
+         mensagensErro.push("Valor inválido.");
+     }
+     
+     // Se houver mensagens de erro, exibe todas
+     if (mensagensErro.length > 0) {
+         exibirErros(mensagensErro);
+         return;
+     }
+    
+    // Se chegou até aqui, os campos estão corretos, então pode adicionar a encomenda
+    adicionarEncomenda(nome, quantidade, produto, valor);
+});
+
+// Função para exibir mensagens de erro no div de erro
+function exibirErros(mensagensErro) {
+    var errorDiv = document.getElementById("error-messages");
+    mensagensErro.forEach(function(mensagem) {
+        var errorMessage = document.createElement("div");
+        errorMessage.textContent = mensagem;
+        errorMessage.style.color = "red";
+        errorDiv.appendChild(errorMessage);
+    });
+}
+
+// Função para adicionar a encomenda na tabela
+function adicionarEncomenda(nome, quantidade, produto, valor) {
+
+    document.getElementById("error-messages").innerHTML = '';
+
+    var table = document.querySelector(".tabela_principal tbody");
+    var newRow = table.insertRow();
+
+    // Criação das células da nova linha
+    var cellNome = newRow.insertCell(0);
+    var cellProduto = newRow.insertCell(1);
+    var cellQuantidade = newRow.insertCell(2);
+    var cellValorUnitario = newRow.insertCell(3);
+    var cellValorTotal = newRow.insertCell(4);
+
+    // Definição do conteúdo das células
+    cellNome.textContent = nome;
+    cellProduto.textContent = produto;
+    cellQuantidade.textContent = quantidade;
+    cellValorUnitario.textContent = formatarValorMonetario(valor);
+
+    // Verificação da validade da quantidade e do valor
+    if (quantidade <= 0 || isNaN(quantidade)) { // Modificado <= 0
+        cellQuantidade.textContent = "Quantidade Inválida";
+        cellQuantidade.style.color = "red";
+        cellValorTotal.textContent = ""; // Limpa o valor total
+    } else if (valor <= 0 || isNaN(valor)) { // Modificado <= 0
+        cellValorUnitario.textContent = "Valor Inválido";
+        newRow.style.backgroundColor = "red";
+        cellNome.style.color = "white";
+        cellProduto.style.color = "white";
+        cellQuantidade.style.color = "white";
+        cellValorUnitario.style.color = "white";
+        cellValorTotal.textContent = ""; // Limpa o valor total
+    } else {
+        // Quantidade e valor válidos, calcula o valor total e formata
+        cellValorTotal.textContent = formatarValorMonetario(calculoTotal(quantidade, valor));
+    }
+
+    // Limpa os campos do formulário
+    document.getElementById("nome").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("produto").selectedIndex = 0;
+    document.getElementById("valor").value = "";
+
+    // Adiciona evento de duplo clique para remover a linha recém-adicionada
+    newRow.addEventListener("dblclick", function() {
+        this.remove();
+    });
+}
+
+function limparMensagensErro() {
+    var errorDiv = document.getElementById("error-messages");
+    errorDiv.innerHTML = ''; // Limpa o conteúdo do div de mensagens de erro
+}
+
+document.querySelectorAll(".linhas").forEach(function(linha) {
+    linha.addEventListener("dblclick", function() {
+        this.remove();
+    });
+});
+
+// Adiciona evento de clique para remover a tabela
+document.querySelector(".tabela_principal").addEventListener("click", function(event) {
+    // Verifica se o elemento clicado é a própria tabela
+    if (event.target.classList.contains("tabela_principal")) {
+        // Remove a tabela
+        this.remove();
+    }
+});
---
+++ scripts/captura-valor.js
@@ -0,0 +1,51 @@
+var titulo = document.querySelector(".titulo_secao");
+titulo.textContent = "Lista de Encomendas";
+
+// Captura todos os clientes que fizeram encomendas 
+var clientes = document.querySelectorAll(".linhas");
+
+// Passa por cada encomenda calculando o valor total 
+for (var count = 0; count < clientes.length; count++) {
+    // Captura a quantidade encomendada
+    var qtde = parseInt(clientes[count].querySelector(".info-qtde").textContent);
+    // Captura o valor unitário do produto 
+    var unitario = parseFloat(clientes[count].querySelector(".info-valor").textContent);
+
+    // Valida a quantidade e o valor unitário
+    if (qtde <= 0 || isNaN(qtde) || unitario <= 0 || isNaN(unitario)) {
+        // Adiciona a mensagem de erro na célula adequada
+        if (qtde <= 0 || isNaN(qtde)) {
+            var errorMessageCell = clientes[count].querySelector(".info-qtde");
+            errorMessageCell.textContent = "Quantidade Inválida";
+            errorMessageCell.style.color = "red";
+        }
+        if (unitario <= 0 || isNaN(unitario)) {
+            var errorMessageCell = clientes[count].querySelector(".info-valor");
+            errorMessageCell.textContent = "Valor Inválido";
+            errorMessageCell.style.color = "red";
+        }
+
+        // Colore a linha em vermelho
+        clientes[count].style.backgroundColor = "red";
+        clientes[count].querySelectorAll("td").forEach(function(cell) {
+            cell.style.color = "white";
+        });
+    } else {
+        // Quantidade e valor unitário válidos
+        // Calcula o valor total da encomenda 
+        clientes[count].querySelector(".info-valor").textContent = formatarValorMonetario(unitario);
+        clientes[count].querySelector(".info-valor-total").textContent = formatarValorMonetario(calculoTotal(qtde, unitario));
+    }
+}
+
+// Função para calcular o valor total de encomendas 
+function calculoTotal(qtde, unitario) {
+    var total = qtde * unitario;
+    return total;
+}
+
+// Função para formatar valores monetários 
+function formatarValorMonetario(valor) {
+    var formReal = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return formReal;
+}
